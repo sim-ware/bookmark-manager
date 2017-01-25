ed658f46bb60d99ff80ae878fe4faa8d9f2aeb47
Samir Gossain <samir.gossain@gmail.com>
2017-01-25 17:46:40 +0000

    Edited first method in controller

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -20,7 +20,7 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
     params[:tags].split(', ').each do |t|
-      link.tags << Tag.create(tag_name: t)
+      link.tags << Tag.first_or_create(tag_name: t)
     end
     link.save
     redirect '/links'
