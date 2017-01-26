e3495d61661ed94a81cc3c5e9183c41f40d2abe1
Samir Gossain <samir.gossain@gmail.com>
2017-01-26 16:27:35 +0000

    Step 19; Writing Tests

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,8 +1,10 @@
 
-def sign_up
-  visit '/signup'
-  expect(page.status_code).to eq(200)
-      fill_in :email, with: 'eric@example.com'
-      fill_in :password, with: 'password1'
-      click_button 'Signup'
+def sign_up(email: 'alice@example.com',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/signup'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Signup'
 end
