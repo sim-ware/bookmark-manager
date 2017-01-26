6fd5923d21410933f4674c7f3a78934628049df6
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-26 16:58:45 +0000

    19: password confirmation

--- bookmark_manager.rb
+++ bookmark_manager.rb
@@ -25,7 +25,8 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/signup' do
-    user = User.create(email: params[:email], password: params[:password])
+    user = User.create(email: params[:email], password: params[:password],
+                    password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect to('/links')
   end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,19 +3,11 @@ require 'spec_helper'
 feature 'Sign up' do
 
   scenario 'new sign up' do
-    visit '/signup'
-    fill_in :email, with: 'eric@example.com'
-    fill_in :password, with: 'password1'
-    click_button 'Signup'
-    expect(page.status_code).to eq 200
     expect { sign_up }.to change(User, :count).by(1)
-
-    within 'p#welcome' do
-      expect(page).to have_content('eric@example.com')
-    end
   end
 
-  scenario 'requires a matching confirmation password' do
+  scenario 'fails if confirmation password doesn\'t match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
+
 end
